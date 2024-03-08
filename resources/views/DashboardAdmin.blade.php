0a8e3439d84398ee929e55f9f7f7de23e20c4929
jonnnyhdz <jonathanhdz262@gmail.com>
2024-03-07 22:16:09 -0500

    Cambios el diseño de dashboard y tabla de alumnos

--- resources/views/DashboardAdmin.blade.php
+++ resources/views/DashboardAdmin.blade.php
@@ -17,7 +17,7 @@
     <div>
         <div class="container mx-auto mt-20">
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-                <a href="/DocumentosAlumnos" class="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-colors duration-300 hover:bg-[#f7f7f7ec]">
+                <a href="/DocumentosAlumnos" class="border-l-4 border-blue-500 bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-gray-50">
                     <div class="flex flex-col items-center justify-center mb-4">
                         <h3 class="text-blue-800 text-2xl font-semibold mb-4">Proyectos</h3>
                         <img src="/proyecto.png" alt="Imagen de Proyecto" class=" h-20 object-cover mb-4">
@@ -25,7 +25,7 @@
                     </div>
                 </a>
 
-                <div class="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-colors duration-300 hover:bg-[#f7f7f7ec]">
+                <div class="border-l-4 border-green-500 bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-gray-50">
                     <div class="flex flex-col items-center justify-center mb-4">
                         <h3 class="text-blue-800 text-2xl font-semibold mb-4">Asesores</h3>
                         <img src="/asesor.png" alt="Imagen de Proyecto" class=" h-20 object-cover mb-4">
@@ -33,26 +33,26 @@
                     </div>
                 </div>
 
-                <div class="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-colors duration-300 hover:bg-[#f7f7f7ec]">
+                <div class="border-l-4 border-yellow-500 bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-gray-50">
                     <div class="flex flex-col items-center justify-center mb-4">
                         <h3 class="text-blue-800 text-2xl font-semibold mb-4">Documentos</h3>
-                        <img src="/documento.png" alt="Imagen de Proyecto" class="h-20 object-cover mb-4">
+                        <img src="/documento.png" alt="Imagen de Proyecto" class=" h-20 object-cover mb-4">
                         <p class="text-gray-600 text-center">Documentos relevantes para la carrera</p>
                     </div>
                 </div>
 
-                <a href="/Alumnos" class="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-colors duration-300 hover:bg-[#f7f7f7ec]">
+                <a href="/Alumnos" class="border-l-4 border-red-500 bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-gray-50">
                     <div class="flex flex-col items-center justify-center mb-4">
                         <h3 class="text-blue-800 text-2xl font-semibold mb-4">Alumnos</h3>
-                        <img src="/alumno.png" alt="Imagen de Proyecto" class="h-20 object-cover mb-4">
+                        <img src="/alumno.png" alt="Imagen de Proyecto" class=" h-20 object-cover mb-4">
                         <p class="text-gray-600 text-center">Información sobre los alumnos</p>
                     </div>
                 </a>
 
-                <div class="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-colors duration-300 hover:bg-[#f7f7f7ec]">
+                <div class="border-l-4 border-purple-500 bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between mx-4 mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-gray-50">
                     <div class="flex flex-col items-center justify-center mb-4">
                         <h3 class="text-blue-800 text-2xl font-semibold mb-4">Roles</h3>
-                        <img src="/rol.png" alt="Imagen de Proyecto" class="h-20 object-cover mb-4">
+                        <img src="/rol.png" alt="Imagen de Proyecto" class=" h-20 object-cover mb-4">
                         <p class="text-gray-600 text-center">Roles y responsabilidades en el sistema</p>
                     </div>
                 </div>
