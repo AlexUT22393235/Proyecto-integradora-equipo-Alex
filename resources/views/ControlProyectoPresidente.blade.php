c2b79593ebd672d3c4394c22cd67e196d481a174
jonnnyhdz <jonathanhdz262@gmail.com>
2024-03-11 11:23:23 -0500

    Correcciones de algunos cruds

--- resources/views/ControlProyectoPresidente.blade.php
+++ resources/views/ControlProyectoPresidente.blade.php
@@ -1,4 +1,4 @@
-@extends('plantillaPresidente')
+@extends('plantilla')
 
 @section('content')
 <!DOCTYPE html>
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modulo 7</title>
+    <title>Proyecto - Presidente</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
         .header-nav a:hover {
@@ -18,7 +18,7 @@
         }
 
         .vote-button:hover {
-            background-color: #dc2626;
+            background-color: #2563eb;
         }
 
         .comment-button:hover {
@@ -26,37 +26,40 @@
         }
     </style>
 </head>
-<div class="text-zinc-400 border-b border-zinc-400 px-5 py-3 text-xl mx-5">
-    Control de proyectos
+
+<div class="p-8">
+<div class="font-bold  border-b border-zinc-400 px-5 py-3 text-2xl mx-5">
+    Detalles de proyecto
 </div>
 <body class="flex flex-col min-h-screen bg-blue-50">
 
+
 <div class="flex-grow mt-5" >
     
-    <div class="flex justify-evenly items-start p-6">
-        <div class="w-full max-w-lg">
-            <div class="flex flex-col items-center mb-6">
+    <div class="flex justify-evenly items-start p-6 ">
+        <div class="w-full max-w-lg ">
+            <div class="flex flex-col items-center mb-6 ">
                 <img src="/project.png" alt="Project" class="w-full max-w-md mb-4">
-                <button class="bg-blue-800 text-white px-6 py-2 rounded shadow hover:bg-blue-700 transition duration-300 project-info">
+                <button class="bg-blue-800 text-white px-6 py-2 rounded-xl shadow hover:bg-blue-700 transition duration-300 project-info">
                     Visualizar Proyecto
                 </button>
             </div>
         </div>
         <div class="flex flex-col w-full max-w-lg gap-4">
-            <div class="bg-blue-100 p-4 text-center rounded shadow">
+            <div class="bg-white p-4 text-center rounded-2xl shadow-md">
                 <h2 class="text-blue-800 text-lg">Asesor Académico</h2>
                 <p>Rafael Villegas</p>
                 <p class="text-red-600">rvillegas@utcancun.edu.mx</p>
             </div>
-            <div class="bg-blue-100 p-4 text-center rounded shadow">
+            <div class="bg-white p-4 text-center rounded-2xl shadow-md">
                 <h2 class="text-blue-800 text-lg">Estatus del proyecto</h2>
                 <p>Comentarios: <span class="text-red-600">2</span></p>
                 <p>Votos: <span class="text-green-600">3</span></p>
-                <button class="bg-red-300 text-white px-4 py-2 rounded-xl shadow-lg flex items-center justify-center mx-auto vote-button">
+                <button class="bg-blue-800 text-white px-4 py-2 rounded-xl shadow-lg flex items-center justify-center mx-auto vote-button">
                     <span class="text-lg mr-2">❤️</span> Votar
                 </button>
             </div>
-            <div class="bg-blue-100 p-4 w-full text-center rounded shadow">
+            <div class="bg-white p-4 text-center rounded-2xl shadow-md">
                 <h2 class="text-blue-800 text-lg">Comentarios:</h2>
                 <p>El documento presentado no tiene ningún aspecto que se tenga que corroborar ya que abarco todos los
                     requerimientos que se tienen que cumplir y tiene <span
@@ -65,6 +68,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 </body>
 </html>
--- resources/views/crudAsesores.blade.php
+++ resources/views/crudAsesores.blade.php
@@ -1,61 +1,117 @@
 @extends('plantilla')
 
 @section('content')
-<div class="p-8">
-    <h1 class="text-2xl font-bold mb-4">Lista de Asesores</h1>
-    <div class="mt-4 text-right">
-        <a href="" class="btn btn-primary bg-sge text-white px-4 py-2 rounded-xl shadow-lg">Agregar Nuevo Asesor</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de Asesores</title>
+</head>
+<body>
+    <main class="p-8">
+        {{-- Titulo --}}
+        <div class="border-b border-zinc-400 px-5 py-3 font-bold text-2xl mx-5">
+        Lista de Asesores
+        </div>
+
+        {{-- Agregar --}}
+        <div class="mt-5 max-w-2xl">
+            <button id="open-modal-btn"
+                type="button"
+                class="block mx-20 bg-[#325b87] text-white px-4 py-2 rounded-md hover:bg-[#1e3349] focus:outline-none focus:bg-[#1e3349]"
+            >
+            Agregar Nuevo Asesor
+            </button>
+        </div>
+
+        {{-- Tabla --}}
+        <div class="mt-5 mx-20 overflow-auto h-250 border-separate border border-gray-300 bg-gray-200 rounded-t-lg items-center">
+            <table class="w-full table-auto bg-gray-200 rounded-t-lg">
+                <thead class="text-center text-white bg-[#325b87]">
+                    <tr>
+                        <th class="p-2">Nombre del Asesor</th>
+                        <th class="p-2">Carrera - División</th>
+                        <th class="p-2">Proyectos Asignados</th>
+                        <th class="p-2">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center bg-white">
+                    @foreach(range(1, 10) as $index)
+                        <tr class= 'border border-gray-200'>
+                            <td>Docente - NA {{ $index }}</td>
+                            <td>Ingenieria - Software</td>
+                            <td>3 Memorias</td>
+                            <td class="pt-2 flex justify-center space-x-10">
+                                <button
+                                    type="button"
+                                    class="px-4 py-2 rounded-full focus:outline-none focus:ring-4 "
+                                >
+                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAcBJREFUSEvt1MtKw0AYBeAzSdulNxRBQcWNiODCrS7E8TmkahsVmlYU9wW3pa1RRB3qwocQoQ2l9YZrH0BdiA/gxkrsSLxQNZem6YiCZpvM/3HOzITghx7yQy6Ewko23/cUkEguRm/qBRIGz2rlrgCMC4BLsoHxnRV664YLgefTRx1SMHgCjuE37KYeLgSOaoUcQOa+JLyWDUw4JRcChzPFtqBc1QGMfcI5X2WJ6bRd5b7hpe3jdsOoqEyl6yCEW3CODZagy0777At+RR+PAYwA/ICpNGziqnbY8kBCJXByyeJ0RujhUlLFTh56qXW0NpjvM5VGTFzZzbf23J3eJ5PJqjDYRBGqnnBgyDq0lrzeHTbfe67aHTVHfQMcyRa6JYmU7JM2jnpK/ILK5IxzDDpUyJg6tWDur5eK379xrXoxrfdWgyi7onGqNALWhU30KYBTAP0Og7dYnKp+UMeqvxt1hKOafgVgwDYNR4Yl6IrfpK5VRzXd/qAQpJhK15pF3RJbYYGod1gw6ghHNvOT73XKhFR2Y9PnIur9OMPzL/MfbrYB26odr5NPjcWpxfldsLKhJ32Gs122l6CWeX/vVD8DNwKwH24kwnEAAAAASUVORK5CYII="/> 
+                                </button>
+                                <button
+                                    type="button"
+                                    class=" px-4 py-2 rounded-full hover:bg-yellow-50 focus:outline-none focus:ring-4"
+                                >
+                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAXpJREFUSEvtlj1Ow0AQhd8saaigiSM6joA4QGK8zgkgEgUIboCE6DE1BRwhCBokOEG8xrkA4gh0KI4ikYom2kFrCUggif8ihcLb7r75Zp531kNY0qIlcZEazK3WSn8wOGMhVqclS1p/VOv1S/I8naaY1OBIymsAJ/OCMvNVLQhOFwaOpDwC0E4TEMC+pdR90tmJivuuazPzU5Iozz4R7VR9P/zS/g/wm21vikrlOE9FSRo9GrU3wvB1asXj4qjZ3CLm9aSAcy8b0bvV6bxM7YJZwshxQhA1ioDB3LWCwC7BxoGZD8hvq5noIhZobf/5BMxdFiJuFWI+/7Z2EVZbSsVJ9lzXmwgOwCRV833P7EdScgkudKtLq8ftKy9X2U6F2slMECYAM5v/tRmFxtcNEcWj0cQEs4gnM9fvMRdYygcAu7mAP6JHS6m9TFb3pDwg4LYImIHDmlJ3mcDmcN9xtiFEQwNrWRIQwBBad6tB8DxLl3qgzwJOc3Zp4E8+h0kuW0jNVgAAAABJRU5ErkJggg=="/> 
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </main>
+
+    <div id="modal" class="fixed inset-0 flex items-center justify-center z-10 hidden">
+        <div class="absolute inset-0 bg-black opacity-75 modal-overlay" onclick="closeModal()"></div>
+        <div class="bg-white p-8 rounded-lg z-20 modal-container w-96">
+            <!-- Ajuste del ancho del modal -->
+            <span id="close-modal-btn" class="close absolute top-0 right-0 p-4">&times;</span>
+            <h2 class="text-lg font-bold mb-4">Agregar Nuevo Asesor</h2>
+            <form>
+                <div class="mb-4">
+                    <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre del Asesor</label>
+                    <input type="text" id="nombre" name="nombre" class="mt-1 p-2 border border-gray-300 rounded-md w-full" required>
+                </div>
+                <div class="mb-4">
+                    <label for="nombre" class="block text-sm font-medium text-gray-700">Carrera - División</label>
+                    <input type="text" id="division" name="division" class="mt-1 p-2 border border-gray-300 rounded-md w-full" required>
+                </div>
+                <div class="mb-4">
+                    <label for="nombre" class="block text-sm font-medium text-gray-700">Proyectos Asignados</label>
+                    <input type="text" id="proyectos" name="proyectos" class="mt-1 p-2 border border-gray-300 rounded-md w-full" required>
+                </div>
+                <div class="flex justify-end">
+                    <button type="submit" class="px-4 py-2 bg-sge text-white rounded-lg">Guardar</button>
+                    <button type="button" class="px-4 py-2 bg-gray-300  text-gray-700 rounded-lg ml-4" onclick="closeModal()">Cancelar</button>
+                </div>
+            </form>
+        </div>
     </div>
 
-    <table class="table-auto w-full mt-4 rounded-t-lg">
-        <thead>
-            <tr class="bg-sge text-white">
-                <th class="px-4 py-2">Cédula</th>
-                <th class="px-4 py-2">Nombre</th>
-                <th class="px-4 py-2">Alumnos</th>
-                <th class="px-4 py-2">Asesorados</th>
-                <th class="px-4 py-2">Estatus</th>
-                <th class="px-4 py-2">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Ejemplo de datos de prueba -->
-            <tr>
-                <td class="border px-4 py-2">123456789</td>
-                <td class="border px-4 py-2">John Doe</td>
-                <td class="border px-4 py-2">20</td>
-                <td class="border px-4 py-2">50</td>
-                <td class="border px-4 py-2">Activo</td>
-                <td class="border px-4 py-2 items-center ">
-                    <span class="min-h-[40px] flex items-center">
-                        <button><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAcBJREFUSEvt1MtKw0AYBeAzSdulNxRBQcWNiODCrS7E8TmkahsVmlYU9wW3pa1RRB3qwocQoQ2l9YZrH0BdiA/gxkrsSLxQNZem6YiCZpvM/3HOzITghx7yQy6Ewko23/cUkEguRm/qBRIGz2rlrgCMC4BLsoHxnRV664YLgefTRx1SMHgCjuE37KYeLgSOaoUcQOa+JLyWDUw4JRcChzPFtqBc1QGMfcI5X2WJ6bRd5b7hpe3jdsOoqEyl6yCEW3CODZagy0777At+RR+PAYwA/ICpNGziqnbY8kBCJXByyeJ0RujhUlLFTh56qXW0NpjvM5VGTFzZzbf23J3eJ5PJqjDYRBGqnnBgyDq0lrzeHTbfe67aHTVHfQMcyRa6JYmU7JM2jnpK/ILK5IxzDDpUyJg6tWDur5eK379xrXoxrfdWgyi7onGqNALWhU30KYBTAP0Og7dYnKp+UMeqvxt1hKOafgVgwDYNR4Yl6IrfpK5VRzXd/qAQpJhK15pF3RJbYYGod1gw6ghHNvOT73XKhFR2Y9PnIur9OMPzL/MfbrYB26odr5NPjcWpxfldsLKhJ32Gs122l6CWeX/vVD8DNwKwH24kwnEAAAAASUVORK5CYII="/></button> 
-                            <button><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAXpJREFUSEvtlj1Ow0AQhd8saaigiSM6joA4QGK8zgkgEgUIboCE6DE1BRwhCBokOEG8xrkA4gh0KI4ikYom2kFrCUggif8ihcLb7r75Zp531kNY0qIlcZEazK3WSn8wOGMhVqclS1p/VOv1S/I8naaY1OBIymsAJ/OCMvNVLQhOFwaOpDwC0E4TEMC+pdR90tmJivuuazPzU5Iozz4R7VR9P/zS/g/wm21vikrlOE9FSRo9GrU3wvB1asXj4qjZ3CLm9aSAcy8b0bvV6bxM7YJZwshxQhA1ioDB3LWCwC7BxoGZD8hvq5noIhZobf/5BMxdFiJuFWI+/7Z2EVZbSsVJ9lzXmwgOwCRV833P7EdScgkudKtLq8ftKy9X2U6F2slMECYAM5v/tRmFxtcNEcWj0cQEs4gnM9fvMRdYygcAu7mAP6JHS6m9TFb3pDwg4LYImIHDmlJ3mcDmcN9xtiFEQwNrWRIQwBBad6tB8DxLl3qgzwJOc3Zp4E8+h0kuW0jNVgAAAABJRU5ErkJggg=="/></button>
-                    </span>
-                </td>
-            </tr>
-            <!-- Fin del ejemplo de datos de prueba -->
+    <!-- JavaScript para controlar el modal -->
+    <script>
+        // Función para abrir el modal
+        function openModal() {
+            document.getElementById('modal').classList.remove('hidden');
+        }
 
-            <!-- Agregar 9 asesores más -->
-            @for ($i = 1; $i <= 9; $i++)
-                <tr>
-                    <td class="border px-4 py-2">Cédula{{ $i }}</td>
-                    <td class="border px-4 py-2">Nombre{{ $i }}</td>
-                    <td class="border px-4 py-2">20</td>
-                    <td class="border px-4 py-2">50</td>
-                    <td class="border px-4 py-2">Activo</td>
-                    <td class="border px-4 py-2 items-center ">
-                        <span class="min-h-[40px] flex items-center">
-                            <button><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAcBJREFUSEvt1MtKw0AYBeAzSdulNxRBQcWNiODCrS7E8TmkahsVmlYU9wW3pa1RRB3qwocQoQ2l9YZrH0BdiA/gxkrsSLxQNZem6YiCZpvM/3HOzITghx7yQy6Ewko23/cUkEguRm/qBRIGz2rlrgCMC4BLsoHxnRV664YLgefTRx1SMHgCjuE37KYeLgSOaoUcQOa+JLyWDUw4JRcChzPFtqBc1QGMfcI5X2WJ6bRd5b7hpe3jdsOoqEyl6yCEW3CODZagy0777At+RR+PAYwA/ICpNGziqnbY8kBCJXByyeJ0RujhUlLFTh56qXW0NpjvM5VGTFzZzbf23J3eJ5PJqjDYRBGqnnBgyDq0lrzeHTbfe67aHTVHfQMcyRa6JYmU7JM2jnpK/ILK5IxzDDpUyJg6tWDur5eK379xrXoxrfdWgyi7onGqNALWhU30KYBTAP0Og7dYnKp+UMeqvxt1hKOafgVgwDYNR4Yl6IrfpK5VRzXd/qAQpJhK15pF3RJbYYGod1gw6ghHNvOT73XKhFR2Y9PnIur9OMPzL/MfbrYB26odr5NPjcWpxfldsLKhJ32Gs122l6CWeX/vVD8DNwKwH24kwnEAAAAASUVORK5CYII="/></button> 
-                                <button><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAXpJREFUSEvtlj1Ow0AQhd8saaigiSM6joA4QGK8zgkgEgUIboCE6DE1BRwhCBokOEG8xrkA4gh0KI4ikYom2kFrCUggif8ihcLb7r75Zp531kNY0qIlcZEazK3WSn8wOGMhVqclS1p/VOv1S/I8naaY1OBIymsAJ/OCMvNVLQhOFwaOpDwC0E4TEMC+pdR90tmJivuuazPzU5Iozz4R7VR9P/zS/g/wm21vikrlOE9FSRo9GrU3wvB1asXj4qjZ3CLm9aSAcy8b0bvV6bxM7YJZwshxQhA1ioDB3LWCwC7BxoGZD8hvq5noIhZobf/5BMxdFiJuFWI+/7Z2EVZbSsVJ9lzXmwgOwCRV833P7EdScgkudKtLq8ftKy9X2U6F2slMECYAM5v/tRmFxtcNEcWj0cQEs4gnM9fvMRdYygcAu7mAP6JHS6m9TFb3pDwg4LYImIHDmlJ3mcDmcN9xtiFEQwNrWRIQwBBad6tB8DxLl3qgzwJOc3Zp4E8+h0kuW0jNVgAAAABJRU5ErkJggg=="/></button>
-                        </span>
-                    </td>
-                </tr>
-            @endfor
-            <!-- Fin de los 9 asesores adicionales -->
-        </tbody>
-    </table>
+        // Función para cerrar el modal
+        function closeModal() {
+            document.getElementById('modal').classList.add('hidden');
+        }
 
-    
-</div>
+        // Evento para abrir el modal cuando se hace clic en el botón "Agregar nuevo Directivo"
+        document.getElementById('open-modal-btn').addEventListener('click', openModal);
 
+        // Evento para cerrar el modal cuando se hace clic en el botón de cierre
+        document.getElementById('close-modal-btn').addEventListener('click', closeModal);
+
+        // Evento para cerrar el modal cuando se hace clic en el botón "Cancelar"
+        document.getElementById('cancel-modal-btn').addEventListener('click', closeModal);
+    </script>
+</body>
+</html>
 @endsection
